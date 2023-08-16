1350a85a2eda72db8baf225cc1075543f0d7793a
Gopi2301 <gopinaths2301@gmail.com>
2023-08-16 23:24:18 +0530

    upto formik handle change

--- src/pages/CreateStudent.jsx
+++ src/pages/CreateStudent.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import Sidebar from '../components/sibebar/Sidebar'
-import FormControl from '@mui/material/FormControl';
+import Form from '@mui/material/FormControl';
 import { Alert, Grid, TextField } from '@mui/material';
 import '../styles/pages/--createstudent.scss'
 import Button from '@mui/material/Button';
 import { API_URL } from '../API/api_url'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+
+
 const CreateStudent = () => {
     const designation = "student"
     const [firstName, setFirstName] = useState();
@@ -15,37 +18,50 @@ const CreateStudent = () => {
     const [courses, setCourses] = useState();
     const [score, setScore] = useState();
     const navigate = useNavigate()
-    const postData = async () => {
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            studentClass: '',
+            courses: '',
+            score: ''
+        }, onSubmit: (values) => postData(values)
+    })
+
+
+    async function postData(values) {
         try {
             await axios.post(API_URL, {
-                designation,
-                firstName,
-                lastName,
-                studentClass,
-                courses,
-                score
-            },
-                alert('Student Added SuccesFully!'), navigate('/studenttable'))
+                designation: 'student',
+                firstName: values.firstName,
+                lastName: values.lastName,
+                studentClass: values.studentClass,
+                courses: values.courses,
+                score: values.score
+
+            },)
+            alert('Student Added SuccesFully!'), navigate('/studenttable');
+
         } catch (error) {
             console.log(error);
         }
 
     }
-    console.log(URL)
+
     return (
 
         <Grid container spacing={2}>
             < Sidebar />
             <Grid item xs={10} className='main'>
-                <FormControl className='form__list'>
-                    <TextField className='form__list--item' onChange={event => setFirstName(event.target.value)} required id="outlined-basic" label="Student Name" variant="outlined" />
-                    <TextField className='form__list--item' onChange={event => setLastName(event.target.value)} required id="outlined-basic" label="Last Name" variant="outlined" />
-                    <TextField className='form__list--item' onChange={event => setStudentClass(event.target.value)} required id="outlined-basic" label="Student Class ( 1 to 10)" variant="outlined" />
-                    <TextField className='form__list--item' onChange={event => setCourses(event.target.value)} id="outlined-basic" label="Additional Courses" variant="outlined" />
-                    <TextField className='form__list--item' onChange={event => setScore(event.target.value)} id="outlined-basic" label="Overall Score %" variant="outlined" />
+                <form className='form__list' onSubmit={formik.handleSubmit}>
+                    <TextField name="firstName" className='form__list--item' onChange={formik.handleChange} value={formik.values.firstName} required id="outlined-basic" label="Student Name" variant="outlined" />
+                    <TextField name="lastName" className='form__list--item' onChange={formik.handleChange} value={formik.values.lastName} required id="outlined-basic" label="Last Name" variant="outlined" />
+                    <TextField name="studentClass" className='form__list--item' onChange={formik.handleChange} value={formik.values.class} required id="outlined-basic" label="Student Class ( 1 to 10)" variant="outlined" />
+                    <TextField name="courses" className='form__list--item' onChange={formik.handleChange} value={formik.values.courses} id="outlined-basic" label="Additional Courses" variant="outlined" />
+                    <TextField name="score" className='form__list--item' onChange={formik.handleChange} value={formik.values.score} id="outlined-basic" label="Overall Score %" variant="outlined" />
 
-                    <Button onClick={postData} variant="contained">Create</Button>
-                </FormControl>
+                    <Button type="submit" variant="contained">Create</Button>
+                </form>
             </Grid>
         </Grid >
 
