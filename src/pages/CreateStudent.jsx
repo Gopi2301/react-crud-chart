0b2595f96fc27c5ca7ab1c6a539d289ffeb1e052
Gopi2301 <gopinaths2301@gmail.com>
2023-08-17 01:06:28 +0530

    formik validation added for create student

--- src/pages/CreateStudent.jsx
+++ src/pages/CreateStudent.jsx
@@ -8,16 +8,17 @@ import { API_URL } from '../API/api_url'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useFormik } from 'formik';
-
+import * as yup from 'yup';
 
 const CreateStudent = () => {
-    const designation = "student"
-    const [firstName, setFirstName] = useState();
-    const [lastName, setLastName] = useState();
-    const [studentClass, setStudentClass] = useState();
-    const [courses, setCourses] = useState();
-    const [score, setScore] = useState();
     const navigate = useNavigate()
+    const validationSchema = yup.object({
+        firstName: yup.string().required(),
+        lastName: yup.string().required(),
+        studentClass: yup.number().min(1).max(10).required('Must be Number 1 to 10'),
+        courses: yup.string(),
+        score: yup.number().min(1).max(99).required()
+    })
     const initialValues = {
         firstName: '',
         lastName: '',
@@ -25,31 +26,12 @@ const CreateStudent = () => {
         courses: '',
         score: ''
     }
-    const validate = values => {
-        let errors = {}
-        if (!values.firstName) {
-            errors.firstName = "Required"
-        }
-        if (!values.lastName) {
-            errors.lastName = "Required"
-        }
-        if (!values.studentClass) {
-            errors.studentClass = "Required"
-        }
-        if (!values.courses) {
-            errors.courses = "Required"
-        }
-        if (!values.score) {
-            errors.score = "Required"
-        }
-        return errors
-    }
+
     const onSubmit = (values) => postData(values);
     const formik = useFormik({
-        initialValues: initialValues, onSubmit: onSubmit,
-        validate: validate
+        initialValues, onSubmit, validationSchema
     })
-    console.log(formik.touched);
+    console.log(formik.errors);
 
 
     async function postData(values) {
@@ -77,13 +59,12 @@ const CreateStudent = () => {
             < Sidebar />
             <Grid item xs={10} className='main'>
                 <form className='form__list' onSubmit={formik.handleSubmit}>
-                    <TextField name="firstName" className='form__list--item' onChange={formik.handleChange} value={formik.values.firstName} required id="outlined-basic" label="Student Name" variant="outlined" helperText={formik.touched.firstName && formik.errors.firstName ? formik.errors.firstName : null} onBlur={formik.handleBlur} />
-                    <TextField name="lastName" className='form__list--item' onChange={formik.handleChange} value={formik.values.lastName} required id="outlined-basic" label="Last Name" variant="outlined" helperText={formik.touched.lastName && formik.errors.lastName ? formik.errors.lastName : null} onBlur={formik.handleBlur} />
-                    <TextField name="studentClass" className='form__list--item' onChange={formik.handleChange} value={formik.values.class} required id="outlined-basic" label="Student Class ( 1 to 10)" variant="outlined" helperText={formik.touched.studentClass && formik.errors.studentClass ? formik.errors.studentClass : null} onBlur={formik.handleBlur} />
+                    <TextField name="firstName" className='form__list--item' onChange={formik.handleChange} value={formik.values.firstName} id="outlined-basic" label="Student Name*" variant="outlined" helperText={formik.touched.firstName && formik.errors.firstName ? formik.errors.firstName : null} onBlur={formik.handleBlur} />
+                    <TextField name="lastName" className='form__list--item' onChange={formik.handleChange} value={formik.values.lastName} id="outlined-basic" label="Last Name*" variant="outlined" helperText={formik.touched.lastName && formik.errors.lastName ? formik.errors.lastName : null} onBlur={formik.handleBlur} />
+                    <TextField name="studentClass" className='form__list--item' onChange={formik.handleChange} value={formik.values.class} id="outlined-basic" label="Student Class*" variant="outlined" helperText={formik.touched.studentClass && formik.errors.studentClass ? formik.errors.studentClass : null} onBlur={formik.handleBlur} />
                     <TextField name="courses" className='form__list--item' onChange={formik.handleChange} value={formik.values.courses} id="outlined-basic" label="Additional Courses" variant="outlined" helperText={formik.touched.courses && formik.errors.courses ? formik.errors.courses : null} onBlur={formik.handleBlur} />
-                    <TextField name="score" className='form__list--item' onChange={formik.handleChange} value={formik.values.score} id="outlined-basic" label="Overall Score %" variant="outlined" helperText={formik.touched.score && formik.errors.score ? formik.errors.score : null} onBlur={formik.handleBlur} />
-
-                    <Button type="submit" variant="contained">Create</Button>
+                    <TextField name="score" className='form__list--item' onChange={formik.handleChange} value={formik.values.score} id="outlined-basic" label="Overall Score %*" variant="outlined" helperText={formik.touched.score && formik.errors.score ? formik.errors.score : null} onBlur={formik.handleBlur} />
+                    <Button type="submit" disabled={!formik.isValid || formik.isSubmitting} variant="contained">Create</Button>
                 </form>
             </Grid>
         </Grid >
